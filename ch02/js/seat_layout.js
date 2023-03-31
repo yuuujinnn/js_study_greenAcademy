5bcc85e0ed08769773806ddaf917d557a7623fd8
yuuujinnn <yujin_0318@naver.com>
2023-03-31 18:17:58 +0900

    배열, 객체

--- ch02/js/seat_layout.js
+++ ch02/js/seat_layout.js
@@ -1,14 +1,14 @@
 // 입장객 수에 따른 좌석의 줄(행)수 계산
-let customer = prompt("입장객은 몇 명인가요? "); //입장객 수
-let colNum = prompt("한 줄에 몇명씩 앉나요? ");    //열의 수
+let customer = parseInt(prompt("입장객은 몇 명인가요? ")); //입장객 수
+let colNum = parseInt(prompt("한 줄에 몇명씩 앉나요? "));    //열의 수
 let rowNum;        //행(줄)의 수
 
 // 나머지가 있는지 없는지 처리
-if(customer == null || colNum == null){
+if(customer == null || colNum == null || isNaN(customer) || isNaN(colNum)){
     document.write("<p>입력이 취소되었습니다.</p>");
 }
 else{
-    if(parseInt(customer) % parseInt(colNum) == 0){
+    if(customer % colNum == 0){
         rowNum = customer / colNum;
     }
     else{
